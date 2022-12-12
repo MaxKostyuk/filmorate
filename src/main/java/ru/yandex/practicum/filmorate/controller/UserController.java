7ae961b0a8169831b89c4e6ebb3d5444db06b377
Max Kostyuk <kostyuk.max@gmail.com>
2022-12-13 02:45:08 +0600

    feat: реализованы классы User и Film, валидация полей при создании, rest контроллеры UserController и FilmController

---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -0,0 +1,58 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.ElementNotFoundException;
+import ru.yandex.practicum.filmorate.model.User;
+
+import javax.validation.Valid;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private Map<Integer, User> userMap = new HashMap<>();
+    private int idCounter = 0;
+
+    @GetMapping
+    private Collection<User> getAllUsers() {
+        return userMap.values();
+    }
+
+    @PostMapping
+    private User createUser(@Valid @RequestBody User user) {
+        user.setId(setNewId());
+        putUserToMap(user, "User with id " + user.getId() + " was added");
+        return user;
+    }
+
+    @PutMapping
+    private User updateUser(@Valid @RequestBody User user) {
+        if (!userMap.containsKey(user.getId()))
+            throw new ElementNotFoundException("User with id " + user.getId() + " not found");
+        putUserToMap(user, "User with id " + user.getId() + " was updated");
+        return user;
+    }
+
+    private void putUserToMap(User user, String message) {
+        if (user.getName() == null | user.getName().isBlank())
+            user.setName(user.getLogin());
+        userMap.put(user.getId(), user);
+        log.info(message);
+    }
+
+    @ExceptionHandler(ElementNotFoundException.class)
+    @ResponseStatus(value = HttpStatus.NOT_FOUND)
+    private void elementNotFoundHandler(ElementNotFoundException e) {
+        log.warn(e.getMessage());
+    }
+
+    private int setNewId() {
+        return ++idCounter;
+    }
+}
---
+++ src/main/java/ru/yandex/practicum/filmorate/exception/ElementNotFoundException.java
@@ -0,0 +1,11 @@
+package ru.yandex.practicum.filmorate.exception;
+
+public class ElementNotFoundException extends RuntimeException {
+    public ElementNotFoundException(String message) {
+        super(message);
+    }
+
+    public ElementNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
